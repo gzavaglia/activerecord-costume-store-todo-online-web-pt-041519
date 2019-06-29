b6da428a1af8af397e9efb2c29341e4131e0a0d4
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 21:35:42 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :haunted_houses do |t|
       t.string :name
