4b6cb2f69cc457c9c2b9cb731eb313d2bdfe8b6d
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 21:24:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,5 +9,8 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
-  create_table 
+  create_table :costumes do |cols|
+    cols.string :name,
+    
+  end
 end
