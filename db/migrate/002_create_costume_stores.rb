a143a0a5d35d76d9daea7e94a32d6ae39b9eaad2
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 21:31:09 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumesStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :still_in_business
+      t.timestamps :opening_time
+      
+    end
+  end
+end
