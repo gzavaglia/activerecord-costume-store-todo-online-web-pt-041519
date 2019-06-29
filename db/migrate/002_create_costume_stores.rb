adb3d98239229a34ef444448ea36eac458bcb164
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 21:36:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,14 @@
-class CreateCostumesStores < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration[5.0]
   def change
-    create_table :costumes_stores do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
   end
 end
+© 2019 GitHub, Inc.
